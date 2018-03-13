6529e611d6a06247db6ffc8c83617ad8de723537
SAW <s.a.walters.jr@gmail.com>
2018-03-13 18:10:05 -0400

    no changes to *html files, updated petigre list for menu-1.3.py

--- OH-Wx/menu-1.3.py
+++ OH-Wx/menu-1.3.py
@@ -55,7 +55,7 @@ from datetime import date, timedelta
 
 class Menu:
 	### GLOBALS ###
-	REPO = 'C:/Storm_Images/NEW---TEMP/DataGrab/'
+	REPO = 'C:\\Users\\anwalters\\Desktop\\OH-Wx\\DataGrab\\'
 	DATE = datetime.datetime.now()
 	
 	# get init time and format date for Current Obs
