3999492bff87df2c33f84302599aed4526968f54
scripts <s.a.walters.jr@gmail.com>
2022-06-02 17:47:20 -0500

    see Change.log

--- MesoScraper-menu.py
+++ MesoScraper-menu.py
@@ -12,15 +12,8 @@ import time
 class Menu:
 	### GLOBALS ###
 
-	REPO = 'W:\\WxEvents\\NEW---TEMP\\DataGrab\\'
-	DATE = None
-	
-	page = None
-	init = None
-	date = None
-	SPCdate = None
-	archive = None
-	
+	REPO = "./mesoscrape.config"
+
 	obs = None
 	stack = None
 	
@@ -28,18 +21,24 @@ class Menu:
 	#
 	# !!!
 	def __init__(self):
-		# get init time and format date for Current Obs
-		Menu.page = urllib.request.urlopen('http://www.spc.noaa.gov/exper/mesoanalysis/new/viewsector.php?sector=20').read()
-		Menu.init = str( BeautifulSoup(Menu.page, 'html.parser').findAll('div', {'id': 'latest'})[0].text ).split()[1]
-		Menu.archive = False
+		# remove old mesoscrape.config
+		os.remove(Menu.REPO)
 		
-		Menu.SPCdate = str( BeautifulSoup(Menu.page, 'html.parser').findAll('div', {'id': 'latest'})[0].text ).split()[0]
-		Menu.DATE = datetime.datetime( int("20"+Menu.SPCdate.split("/")[2]), int(Menu.SPCdate.split("/")[0]),int(Menu.SPCdate.split("/")[1]) )
-		Menu.date = datetime.datetime.now()
+		# parameter list to write to config file
+		Menu.obs = []
 		
-		Menu.obs = {'haz':None, 'sec':None, 'day':Menu.date, 'ini':Menu.init}
+		# stack of menu selections for Back()
 		Menu.stack =[]
 		
+	
+	def write_config(line):
+		with open("mesoscrape.config", "a") as f:
+			for i in line:
+				f.write(i + " ")
+			f.write('\n')
+			f.close()
+		
+	
 	
 	### !!! ERROR with stack, Back must be selected twice !!! ###
 	def back():
@@ -90,7 +89,7 @@ class Menu:
 		
 		print( '###############################' )
 		print( '#							  #' )
-		print( '# Wx Data Gather - '+Menu.DATE.strftime('%m/%d/%Y')+' #' )
+		print( '# Wx Data Gather - 			  #' )
 		print( '#							  #' )
 		print( '###############################' )
 		print()
@@ -142,36 +141,26 @@ class Menu:
 			os.system('cls')
 			return Menu.back()
 		else:
-			Menu.obs['haz'] = sel
-			Menu.stack.append(Menu.sec_menu)
+			Menu.write_config(sel)
 			os.system('cls')
 			return Menu.sec_menu()
 		
 	### bad formatting with new hazard value ###
 	def sec_menu():
-		'''options = {'1':('MW',Menu.day_menu),
-				   '2':('EC',Menu.day_menu),
-				   '3':('CP',Menu.day_menu),
-				   '4':('NE',Menu.day_menu),
-				   '5':('US',Menu.day_menu),
-				   '6':('SP',Menu.day_menu),
-				   '7':('SE',Menu.day_menu),
-				   '9':('Back',Menu.back)}'''
-				   
-		sector	= {'SP':['SP','s15'],
-				   'NE':['NE','s16'],
-				   'EC':['EC','s17'],
-				   'SE':['SE','s18'],
-				   'US':['US','s19'],
-				   'MW':['MW','s20'],
-				   'CP':['CP','s14'],
-				   'NP':['NP','s13'],
-				   'SW':['SW','s12'],
-				   'NW':['NW','s11']}
+		sector	= {'SP':'s15',
+				   'NE':'s16',
+				   'EC':'s17',
+				   'SE':'s18',
+				   'US':'s19',
+				   'MW':'s20',
+				   'CP':'s14',
+				   'NP':'s13',
+				   'SW':'s12',
+				   'NW':'s11'}
 		sectors = []
 
 		print( '#############' )
-		print( '# {haz} Wx #'.format(haz='Winter' if Menu.obs['haz'][0]=='2' else 'Severe') )
+		print( '# {haz} Wx  #' )
 		print( '#############' )
 		print()
 		print( 'Select Sector' )
@@ -203,12 +192,13 @@ class Menu:
 		
 		for s in inp:
 			sectors.append(sector[s])
-		
-		Menu.obs['sec'] = sectors
-		os.system('cls')
-		return Menu.day_menu()
-		
 
+		os.system('cls')
+		
+		return Menu.write_config(sectors)
+		
+	
+	'''
 	### bad formatting with new hazard value ###
 	def day_menu():
 		options = {'1':('Today',Menu.aut_menu)}
@@ -239,113 +229,15 @@ class Menu:
 			Menu.obs['day'] = Menu.DATE + timedelta(days = -(int(sel)-1))
 
 		return options[sel][1]()
+	'''	
 		
-	
-	def aut_menu():
-		begt = None
-		dur = None
-		endt = None
 		
-		# TEMPORARY #
-		satlat = 31.32
-		satlon = -97.18
-		sectors = ['TX','DIX','OHV','NPL']	# array of MesoNet sector(s)
+def main():
+		#while(True):
+		Menu()
+		Menu.main_menu()
+		print('\nData Grab Complete')
+		input('\nPress Enter\n')
 
-		
-				   
-		print( '###############' )
-		print( '# {haz} -{sec}- #'.format(haz='Winter' if Menu.obs['haz'][0]=='2' else 'Severe', sec=Menu.obs['sec']) )
-		print( '#  ' + Menu.obs['day'].strftime('%m/%d/%Y') + ' #' )
-		print( '###############' )
-		print()
-		print( 'Press Enter for manual initial times' )
-		print( 'B. Back' )
-		print()
-		
-		print( 'Enter selection above' )
-		print( 'For automatic data grab, enter start hour (24hr Central Time):' )
-		i = input('>> ').upper()
-		
-		# determine selection; UGLY
-		if ( i == '' ):
-			os.system('cls')
-			return Menu.ini_menu()
-		elif ( i == 'B' ):
-			os.system('cls')
-			return Menu.back()
-		else:
-			i = int(i)
-			begt = datetime.datetime(Menu.date.year, Menu.date.month, Menu.date.day, i, 0, 0)
-			
-			# input duration
-			print()
-			print( 'Enter duration in hours:' )
-			dur = int( input('>> ') )
-			
-			### ERRORS - challenges in grabbing dynamic content from msfc.nasa.gov ###
-			#print()
-			#print( 'Enter URL for satellite grab or leave blank' )
-			#saturl = input('>> ')
-			
-			# calculate end time from begin time and duration
-			endt = begt + timedelta(hours =+ dur)
-			curt = datetime.datetime.now()
-			
-			# run until current time exceeds end time
-			while( curt <= endt ):
-						
-				# grab data after current time exceeds begin time, only grab data 45min past the hour
-				if( curt >= begt):
-					if( curt.minute == 45 ):
-						# update initial time parameters to change file name and force default initial time when grabbing data
-						Menu.page = urllib.request.urlopen('http://www.spc.noaa.gov/exper/mesoanalysis/new/viewsector.php?sector=20').read()
-						Menu.init = str( BeautifulSoup(Menu.page, 'html.parser').findAll('div', {'id': 'latest'})[0].text ).split()[1]
-						Menu.obs['ini'] = ''
-						
-						# get proper date if automation runs into new day
-						Menu.SPCdate = str( BeautifulSoup(Menu.page, 'html.parser').findAll('div', {'id': 'latest'})[0].text ).split()[0]
-						Menu.obs['day'] = datetime.datetime( int("20"+Menu.SPCdate.split("/")[2]), int(Menu.SPCdate.split("/")[0]),int(Menu.SPCdate.split("/")[1]) )#.strftime('%Y%m%d')
-					
-						Menu.get_obs()
-						Menu.get_mesonet( sectors, curt+timedelta(hours =+ 5) )		# convert to UTC !!! must update for DST
-						
-						#Menu.get_goes16(satlat,satlon)
-						
-						time.sleep(3300)	# sleep for 55min until next cycle
-					
-					'''
-					if( (curt.minute%5) == 0 ):
-						#Menu.get_goes16(satlat, satlon)
-						time.sleep(65)	# only grab goes16 once per 5min
-					'''
-					
-				curt = datetime.datetime.now()
-		
-		
-	def ini_menu():
-		print( '###############' )
-		print( '# {haz} -{sec}- #'.format(haz='Winter' if Menu.obs['haz'][0]=='2' else 'Severe', sec=Menu.obs['sec']) )
-		print( '#  ' + Menu.obs['day'].strftime('%m/%d/%Y') + ' #' )
-		print( '###############' )
-		print()
-		print( 'Enter Initial Time(s)' )
-		print( 'Sequence times by "begin-end"' )
-		print( 'Or press enter for Current ~{init}Z'.format(init=Menu.init) )
 
-		i = input('\n>> ')
-		'''
-		if i == '9':
-			Menu.back()
-		else:
-			Menu.obs['ini'] = i
-		'''
-		i = i.split('-')
-		if len(i) > 1:
-			for n in range(int(i[0]),int(i[1])+1):
-				Menu.obs['ini'] = str(n)
-				Menu.get_obs()
-		else:
-			Menu.obs['ini'] = str(i[0])
-			return Menu.get_obs()
-		
-		return
+main()
